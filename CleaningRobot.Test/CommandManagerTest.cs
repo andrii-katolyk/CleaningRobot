8f2d6b5d2ad1c91b6505d21ca59dd216fa3f875e
andrii-katolyk <katholyk.a@gmail.com>
2018-12-02 18:04:12 +0200

    CR: Add CommandManager Input parameters Tests

--- CleaningRobot.Test/CommandManagerTest.cs
+++ CleaningRobot.Test/CommandManagerTest.cs
@@ -7,6 +7,29 @@ namespace CleaningRobot.Test
 	public class CommandManagerTest
 	{
 		[TestMethod]
+		public void CommandManager_AddZeroCommands_CommandsSetIsComplete()
+		{
+			CommandManager manager = new CommandManager();
+
+			manager.AddInputCommand("0");
+			manager.AddInputCommand("10 22");
+			
+			Assert.IsTrue(manager.IsCommandsSetComplete);
+		}
+
+		[TestMethod]
+		public void CommandManager_AddZeroCommandsCount_AddOneCommand_CommandsSetIsComplete()
+		{
+			CommandManager manager = new CommandManager();
+
+			manager.AddInputCommand("0");
+			manager.AddInputCommand("10 22");
+			manager.AddInputCommand("N 1");
+
+			Assert.IsTrue(manager.IsCommandsSetComplete);
+		}
+
+		[TestMethod]
 		public void CommandManager_AddTwoCommands_CommandsSetIsComplete()
 		{
 			CommandManager manager = new CommandManager();
@@ -17,6 +40,35 @@ namespace CleaningRobot.Test
 			manager.AddInputCommand("N 1");
 
 			Assert.IsTrue(manager.IsCommandsSetComplete);
+		}
+
+		[TestMethod]
+		public void CommandManager_AddTenThousendsCommands_CommandsSetIsComplete()
+		{
+			CommandManager manager = new CommandManager();
+
+			manager.AddInputCommand("10000");
+			manager.AddInputCommand("10 22");
+
+			for (var i = 0; i <= 10000; i++)
+			{
+				manager.AddInputCommand("N 1");
+			}
+			
+			Assert.IsTrue(manager.IsCommandsSetComplete);
+		}
+
+		[TestMethod]
+		public void CommandManager_AddFiveCommandsCount_AddTwo_CommandsSetIsNotComplete()
+		{
+			CommandManager manager = new CommandManager();
+
+			manager.AddInputCommand("5");
+			manager.AddInputCommand("10 22");
+			manager.AddInputCommand("E 2");
+			manager.AddInputCommand("N 1");
+
+			Assert.IsFalse(manager.IsCommandsSetComplete);
 		}
 	}
 }
